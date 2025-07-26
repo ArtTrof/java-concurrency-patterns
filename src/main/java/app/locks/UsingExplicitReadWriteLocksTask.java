c860f15b38ef39f5a26dcaead85d1e44227dcec7
ArtTrof <randallsng@gmail.com>
2025-07-26 13:21:42 +0200

    using explicit read write locks

---
+++ src/main/java/app/locks/UsingExplicitReadWriteLocksTask.java
@@ -0,0 +1,73 @@
+package app.locks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Write a class ConfigStore:
+ * <p>
+ * Field: Map<String, String> config
+ * <p>
+ * Method: String get(String key) → guarded with readLock
+ * <p>
+ * Method: void put(String key, String value) → guarded with writeLock
+ * <p>
+ * Simulate 50 concurrent reads and 5 writes using ExecutorService
+ * <p>
+ * At the end, print all final keys and values
+ */
+public class UsingExplicitReadWriteLocksTask {
+    private static class ConfigStore {
+        private final Map<String, String> config = new ConcurrentHashMap<>();
+        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+        public String get(String key) {
+            readWriteLock.readLock().lock();
+            try {
+                System.out.println("Thread: " + Thread.currentThread().getName()+" is getting read lock");
+                return config.get(key);
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        }
+
+        public void set(String key, String value) {
+            readWriteLock.writeLock().lock();
+            try {
+                System.err.println("Setting key" + key + " ,value " + value+ " \n Thread: " + Thread.currentThread().getName());
+                config.put(key, value);
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        }
+
+        {
+            config.put("1","1");
+            config.put("2","2");
+        }
+    }
+
+    public static void main(String[] args) {
+        var executors = Executors.newFixedThreadPool(10);
+        var configStore = new ConfigStore();
+
+        for (int i = 0; i < 50; i++) {
+            executors.execute(()->{
+                configStore.get("1");
+            });
+        }
+
+        for (int i = 0; i < 5; i++) {
+            executors.execute(()->{
+                Integer integer = new Random().nextInt();
+                configStore.set(String.valueOf(integer),String.valueOf(integer));
+            });
+        }
+
+        executors.shutdown();
+    }
+}
